c89e641982ddd0ebf98ebbc0db065ca048defce5
Oleg Reekee <reekee94@gmail.com>
2021-01-10 17:17:46 +0100

    'back_ready,initing_front'

---
+++ config/default.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI: "mongodb://admin:admin@dev-shard-00-00.wlh7n.mongodb.net:27017,dev-shard-00-01.wlh7n.mongodb.net:27017,dev-shard-00-02.wlh7n.mongodb.net:27017/dev?ssl=true&replicaSet=atlas-12rtw9-shard-0&authSource=admin&retryWrites=true&w=majority",
+  secretOrKey: 'secret',
+  jwtSecret: "mysecrettoken",
+  githubSecret: '00f31df246ff394c0e167d12c2ce96173cd7cbea',
+  githubClientId: 'd815b979868da8be96e8'
+};
